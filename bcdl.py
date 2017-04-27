1780fea5e5aa99f9395a1d21478c1c810399727a
Johannes Hofmeister <github@account.cessor.de>
2017-04-27 12:42:34 +0200

    Refactored the code a little

--- bcdl.py
+++ bcdl.py
@@ -1,7 +1,7 @@
 import json
 import os
-import requests
 import re
+import requests
 import sys
 
 
@@ -21,6 +21,12 @@ def fs_friendly(filename):
     ])
 
 
+def add_schema_if_necessary(url):
+    if not url.startswith('http:'):
+        url = 'http:' + url
+    return url
+
+
 class Track(object):
 
     def __init__(self, json):
@@ -38,8 +44,7 @@ class Track(object):
     def download(self):
         if self.binary_data is None:
             url = self.json["file"]["mp3-128"]
-            if not url.startswith('http:'):
-                url = 'http:' + url
+            url = add_schema_if_necessary(url)
             self.binary_data = requests.get(url).content
 
 
@@ -49,39 +54,71 @@ class Album(object):
         self.tracks = tracks
         self.artist = artist
         self.title = title
-        self.directory = self.artist + "/" + self.title
+
+    @property
+    def directory(self):
+        return os.path.join(
+            fs_friendly(self.artist),
+            fs_friendly(self.title)
+        )
 
     def download(self):
         print("=========================================")
-        directory = fs_friendly(self.directory)
+        directory = self.directory
         print("Starting download: " + directory)
         if not os.path.exists(directory):
             os.makedirs(directory)
+
         for track in self.tracks:
+            path = directory + "/" + track.filename()
+            if os.path.exists(path):
+                print("  [skipping " + track.filename() + "]")
+                continue
             print("  [downloading " + track.filename() + "]")
             track.download()
-            with open(directory + "/" + track.filename(), 'wb') as f:
+            with open(path, 'wb') as f:
                 f.write(track.binary_data)
         print("=========================================")
 
 
-def parse_tracks(html):
-    raw_json = re.search(r'trackinfo: (?P<tracks>.*),', html).group('tracks')
-    json_tracks = json.loads(raw_json)
-    return (Track(json_track) for json_track in json_tracks)
+FIELD = r'%s: "(?P<%s>.*)",'
+TRACK = r'%s: (?P<%s>.*),'
 
 
-def parse_album_from(html):
-    artist = re.search(r'artist: "(?P<artist>.*)",', html).group('artist')
-    title = re.search(r'album_title: "(?P<title>.*)",', html).group('title')
-    tracks = parse_tracks(html)
-    return Album(artist, title, tracks)
+class Page(object):
+
+    def __init__(self, text):
+        self._text = text
+
+    def _find(self, pattern, field):
+        match = re.search(pattern % (field, field), self._text)
+        return match.group(field)
+
+    def album(self):
+        html = self._text
+        artist = self._find(FIELD, 'artist')
+        title = self._find(FIELD, 'album_title')
+        tracks = self.tracks()
+        return Album(artist, title, tracks)
+
+    def tracks(self):
+        html = self._text
+        raw_json = self._find(TRACK, 'trackinfo')
+        json_tracks = json.loads(raw_json)
+        return (
+            Track(json_track)
+            for json_track
+            in json_tracks
+        )
 
 
 def main():
-    html = requests.get(sys.argv[1]).text
-    album = parse_album_from(html)
-    album.download()
+    url = sys.argv[1]
+    url = url.strip()
+    print('Downloading ' + url)
+    text = requests.get(url).text
+    Page(text).album().download()
+
 
 if __name__ == "__main__":
     main()
